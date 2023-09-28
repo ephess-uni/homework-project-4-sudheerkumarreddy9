ccdeb34c336b265a11acca6aef3abb72efbbf99c
sudheerkumarreddy9 <143284927+sudheerkumarreddy9@users.noreply.github.com>
2023-09-27 22:30:05 -0500

    Update hp_4.py

--- src/hp_4.py
+++ src/hp_4.py
@@ -8,27 +8,53 @@ from collections import defaultdict
 def reformat_dates(old_dates):
     """Accepts a list of date strings in format yyyy-mm-dd, re-formats each
     element to a format dd mmm yyyy--01 Jan 2001."""
-    pass
+    new_dates = []
+    for date in old_dates:
+        new_dates.append(datetime.strptime(date,'%Y-%m-%d').strftime('%d %b %Y'))
 
 
 def date_range(start, n):
     """For input date string `start`, with format 'yyyy-mm-dd', returns
     a list of of `n` datetime objects starting at `start` where each
     element in the list is one day after the previous."""
-    pass
+    if not isinsatnce(n,int) or if not isinatance(start,str):
+        raise TypeError
+    dates_list = []
+    for i in range(0,n):
+        dates_list.append(datetime.strftime(start,'%Y-%m-%d') + timedelta(days=i))
+    return dates_list
+        
 
 
 def add_date_range(values, start_date):
     """Adds a daily date range to the list `values` beginning with
     `start_date`.  The date, value pairs are returned as tuples
     in the returned list."""
-    pass
+    dates_list = []
+    for pos,val in enumerate(values):
+        dates_list.append((datetime.strftime(start_date,'%Y-%m-%d') + timedelta(days=pos),val))
+    return dates_list
 
 
 def fees_report(infile, outfile):
     """Calculates late fees per patron id and writes a summary report to
     outfile."""
-    pass
+    with open(infile) as f:
+        li = []
+        dictreader_obj = DictReader(f)
+        for  item in dictreader_obj:
+            new_dict = {}
+            days_obj = datetime.strptime(item['date_returned'],'%m/%d/%Y') -  datetime.strptime(item['date_due'],'%m/%d/%Y')
+            if days_obj.days > 0:
+                amount = days_obj.days * 0.25
+                new_dict['patron_id'] = item['patron_id']
+                new_dict['late_fees'] = str(round(amount,2))
+                li.append(new_dict)
+    with open(outfile,'w',newline='') as file:
+        writer = DictWriter(file,['patron_id','late_fees'])
+        writer.writeheader()
+        writer.writerow(li)
+    
 
 
 # The following main selection block will only run when you choose
